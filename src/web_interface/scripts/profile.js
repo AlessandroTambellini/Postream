25894912b8229fc5ee267e772fc035c1030ce2fd
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-08-06 16:44:55 +0200

    improve naming

--- src/web_interface/scripts/profile.js
+++ src/web_interface/scripts/profile.js
@@ -4,9 +4,9 @@ I felt the page was without an identity. It was not clear it is a profile page. 
 
 const rand_int = (max) => Math.floor(Math.random() * max + 1);
 
-const profile_icon = document.querySelector('#profile-icon');
+const profile_picture = document.querySelector('#profile-picture');
 
-const profile_face = profile_icon.querySelector('img');
+const profile_face = profile_picture.querySelector('img');
 profile_face.style.top = `${Math.max(20, rand_int(250))}px`
 profile_face.style.left = `${Math.max(10, rand_int(250))}px`
 
@@ -22,5 +22,5 @@ for (let i = 0; i < num_of_circles; i++)
     circle.style.top = `${rand_int(270)}px`;
     circle.style.left = `${rand_int(270)}px`;
 
-    profile_icon.appendChild(circle)
+    profile_picture.appendChild(circle)
 }
