c4d834e1cfa447bd03ffd569c2e9c17f04c74c98
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-08-23 14:59:29 +0200

    add notification deletion effect

--- src/web_interface/scripts/notifications.js
+++ src/web_interface/scripts/notifications.js
@@ -11,10 +11,20 @@ document.querySelectorAll('.delete-notification-btn').forEach(btn => {
         const notification_id = btn.id.split('-')[1];
         const { status_code, payload, req_error } = await req('api/user/notifications', 'DELETE', { id: notification_id });
     
-        if (req_error) {
+        const notification_card = document.querySelector(`#notification-card-${notification_id}`);
+        notification_card.classList.add('deleting');
+        btn.disabled = true;
+
+        if (req_error) 
+        {
+            notification_card.classList.remove('deleting');
+            btn.disabled = false;
             feedback.show('error', err_msg(status_code, 'notification', 'delete'));
-        } else {
-            document.querySelector(`#notification-card-${notification_id}`).remove()
+        } 
+        else {
+            setTimeout(() => {
+                notification_card.remove();
+            }, 300); // Matches the .notification-card transition duration
         }
     });
 });
